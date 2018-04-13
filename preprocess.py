c1e3f25b562fa9f9aa2751ac33df0f30c63c1c88
zhaohh <966986@qq.com>
2018-04-13 14:33:13 +0800

    add tfidf

--- preprocess.py
+++ preprocess.py
@@ -1,2 +1,94 @@
-def remove_html():
-	pass
+import jieba
+import jieba.analyse
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+from bs4 import BeautifulSoup
+import gensim
+
+from datasource import sqliteEngine, News
+from utils import is_chinese_string, load_chinese_stopwords
+
+def article2wordarray(article):
+	stopwords = load_chinese_stopwords()
+
+	# 分词
+	seg_list = jieba.cut(article, cut_all=False)
+	
+	# 按句子形式输出，去除英文
+	word_list = []
+	for seg in seg_list:
+		if is_chinese_string(seg) and seg not in stopwords:
+			word_list.append(seg)
+	return word_list
+
+def articles2sentence():
+	DBSession = sessionmaker(bind=sqliteEngine)
+	session = DBSession()
+
+	stopwords = load_chinese_stopwords()
+
+	out_put_file = "wordarray.txt"
+	f = open(out_put_file, "w")
+		
+	news_list = session.query(News).all()#.limit(1)
+	
+	for news in news_list:
+		sentence_count = 0
+
+		bs = BeautifulSoup(news.content, "html.parser")
+		text = bs.get_text()
+
+		# 分词
+		seg_list = jieba.cut(text, cut_all=False)
+		#print("/ ".join(seg_list))
+
+		# 按句子形式输出，去除英文
+		sentence = []
+		for seg in seg_list:
+			if seg == "。" or seg == "！" or seg == "？":
+				sentence_count = sentence_count + 1
+				#print("/".join(sentence))
+				#for word in sentence:
+				#	print("%s " % word, end='')
+				if len(sentence) > 0:
+					f.writelines(" ".join(sentence))
+					f.write("\n")
+				sentence = []
+			else:
+				if is_chinese_string(seg) and seg not in stopwords:
+					sentence.append(seg)
+		print("news id: %d, sentence_count: %d" % (news.id, sentence_count))
+	f.close()
+
+def articles2array():
+	DBSession = sessionmaker(bind=sqliteEngine)
+	session = DBSession()
+
+	stopwords = load_chinese_stopwords()
+
+	out_put_file = "articlearray.txt"
+	f = open(out_put_file, "w")
+		
+	news_list = session.query(News).all()#.limit(1)
+	
+	for news in news_list:
+		bs = BeautifulSoup(news.content, "html.parser")
+		text = bs.get_text()
+
+		# 分词
+		seg_list = jieba.cut(text, cut_all=False)
+		#print("/ ".join(seg_list))
+
+		# 按句子形式输出，去除英文
+		sentence = []
+
+		for seg in seg_list:
+			if is_chinese_string(seg) and seg not in stopwords:
+				sentence.append(seg)
+		if len(sentence) > 0:
+			f.writelines(" ".join(sentence))
+		f.write("\n")
+		print("news id: %d" % (news.id))
+	f.close()
--- training.py
+++ training.py
@@ -1,45 +1,30 @@
-import jieba
-import jieba.analyse
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker
-from bs4 import BeautifulSoup
 import gensim
 
-from datasource import sqliteEngine, News
-from utils import is_chinese_string, load_chinese_stopwords
+import datasource
 
-def word2vector():
-	DBSession = sessionmaker(bind=sqliteEngine)
-	session = DBSession()
-
-	stopwords = load_chinese_stopwords()
-	sentence_list = []
-
-	news_list = session.query(News).all()#.limit(1)
-	for news in news_list:
-		bs = BeautifulSoup(news.content, "html.parser")
-		text = bs.get_text()
-
-		# 分词
-		seg_list = jieba.cut(text, cut_all=False)
-		#print("/ ".join(seg_list))
-
-		# 按句子形式输出，去除英文
-		sentence = []
-		for seg in seg_list:
-			if seg == "。" or seg == "！" or seg == "？":
-				#print("/".join(sentence))
-				for word in sentence:
-					print("%s " % word, end='')
-				if len(sentence) > 0:
-					sentence_list.append(sentence)
-				sentence = []
-			else:
-				if is_chinese_string(seg) and seg not in stopwords:
-					sentence.append(seg)
-	
-	print('开启训练模型...')
+def train_word2vector_model():
+	sentence_list = datasource.load_training_sentence_array()
+	print('开始训练模型...')
 	model = gensim.models.Word2Vec(sentence_list, min_count=1)
 	model.save('wordvector.model')
+
+def load_word2vector_model():
+	model = gensim.models.Word2Vec.load('wordvector.model')
+	return model
+
+def train_tfidf_model():
+	corpora_documents = datasource.load_training_article_array()
+
+	# get dictionary from corpora_documents
+	dictionary = gensim.corpora.Dictionary(corpora_documents)
+	print(dictionary.token2id)
+
+	bow_corpus = [dictionary.doc2bow(text) for text in corpora_documents]
+
+	# train the tfidf model
+	tfidf_model = gensim.models.TfidfModel(bow_corpus)
+	tfidf_model.save("tfidf.model")
+
+def load_tfidf_model():
+	tfidf = gensim.models.TfidfModel.load("tfidf.model")
+	return tfidf
